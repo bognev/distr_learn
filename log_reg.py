a9acafee0e6b7830d6ad2c6a31c07dfd15b0df97
zabolotsky <bognev@mail.ru>
2019-09-06 10:36:18 +0300

    LR finished

--- log_reg.py
+++ log_reg.py
@@ -2,6 +2,10 @@ import mnist
 from PIL import Image
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+from matplotlib import style
+style.use("ggplot")
 
 #load datasets using https://github.com/datapythonista/mnist
 train_images = mnist.train_images()
@@ -37,67 +41,54 @@ print(test_labels_01.shape )
 print(test_images_01.shape )
 
 class Log_Reg:
-    def __init__(self, n_features, lr, num_iter):
-        self.w = np.zeros(1, n_features)
-        self.b = 0
+    def __init__(self, lr, num_iter):
+        self.lr = lr
+        self.num_iter = num_iter
 
     def sigmoid(self, x):
         return 1/(1+np.exp(-x))
 
-    def calc_grad(self, X, Y):
-        m = X.shape[0]
-
-        final_result = self.sigmoid(np.dot(self.w,X.T) + self.b)
-        Y_T = Y.T
-        cost = (-1/m)*(np.sum(Y_T*np.log(final_result)) + ((1 - Y_T)*(np.log(1-final_result))))
-
-        self.dw = (1/m)*(np.dot(X.T, (final_result - Y.T).T))
-        self.db = (1/m)*(np.sum(final_result - Y.T))
-
-        # grads = {"dw" : dw, "db" : db}
-
-        return cost
-
-    def model_predict(self, X_train, y_train):
-        costs = []
+    def fit(self, X_train, y_train):
+        epsilon = 1e-5
+        m = X_train.shape[1]
+        self.w = np.zeros((1, m))
+        self.b = 0
+        self.costs = np.zeros(self.num_iter)
+        y_T = y_train.T
+        X_T = X_train.T
         for i in range(self.num_iter):
-            grad, cost = self.calc_grad(X_train, y_train)
-            self.dw = grad["dw"]
-            self.db = grad["db"]
+            y_estimation = self.sigmoid(np.dot(self.w, X_T) + self.b)
+            cost = (-1 / m) * (np.sum(y_T * np.log(y_estimation + epsilon) + (1 - y_T) * (np.log(1 - y_estimation + epsilon))))
+            self.dw = (1 / m) * (np.dot(X_T, (y_estimation - y_T).T))
+            self.db = (1 / m) * (np.sum(y_estimation - y_T))
             self.w = self.w - self.lr * self.dw.T
             self.b = self.b - self.lr * self.db
+            self.costs[i] = cost
+        return self.costs
 
-            if(i%128 == 0):
-                costs.append(cost)
-
-        coeff = {"w" : self.w, "b" : self.b}
-        gradient = {"dw" : self.dw, "db" : self.db}
-
-        return coeff, gradient, costs
-
-    def predict(self, final_pred, m):
-        y_pred = np.zeros((1,m))
-        for i in range(final_pred.shape[1]):
-            if( final_pred[0][i] > 0.5):
-                y_pred[0][i] = 1
-        return y_pred
-
-    def fit(self, X_train, y_train):
-        n_features = X_train.shape[1]
-        print('Number of Features', n_features)
-        w, b = self.weight_initialization(n_features)
-        coeff, gradient, costs = self.model_predict(X_train, y_train)
-        w = coeff["w"]
-        b = coeff["b"]
-        final_train_pred = self.sigmoid(np.dot(w, X_train.T) + b)
-        # final_test_pred = self.
-        y_pred = self.predict(final_train_pred, X_train.shape[0])
-        return self
+    def predict(self, X_test, y_test):
+        y_T = y_test.T
+        X_T = X_test.T
+        y_pred = self.sigmoid(np.dot(self.w, X_T) + self.b)
+        y_pred_bin = np.zeros(y_pred.shape[1])
+        for i in range(y_pred.shape[1]):
+            if (y_pred[0][i] > 0.5):
+                y_pred_bin[i] = 1
+        return y_pred_bin
 
 
 log_reg_m = Log_Reg(lr=0.0001, num_iter=100)
-log_reg_m.fit(X_train=train_images_01, y_train=train_labels_01)
-
+costs = log_reg_m.fit(X_train=train_images_01, y_train=train_labels_01)
+plt.plot(costs)
+plt.ylabel('cost')
+plt.xlabel('iterations')
+plt.title('Cost reduction over time')
+plt.show()
+y_train = log_reg_m.predict(train_images_01, train_labels_01)
+print('Training Accuracy',accuracy_score(y_train, train_labels_01))
+#
+y_test = log_reg_m.predict(test_images_01, test_labels_01)
+print('Test Accuracy',accuracy_score(y_test, test_labels_01))
 
 
 
