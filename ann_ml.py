50a750f2e11f9fee9608de0d5e701d4d4329d92b
zabolotsky <bognev@mail.ru>
2019-11-22 14:04:32 +0000

    Some little changes

--- ann_ml.py
+++ ann_ml.py
@@ -7,6 +7,7 @@ import pickle
 import matplotlib.pyplot as plt
 from matplotlib import style
 from scipy.signal import savgol_filter
+from sklearn.preprocessing import StandardScaler
 
 style.use("ggplot")
 import sys
@@ -124,10 +125,10 @@ class FCN:
         lr = self.config.get("learning_rate")
         dr = self.config.get("decay_rate")
         self.v[name] = dr * self.v[name] + (1 - dr) * dw**2
-        w = w - lr*dw/(np.sqrt(self.v[name])+1e-8)
+        w = w - lr * dw / (np.sqrt(self.v[name])+1e-8)
         return w
 
-    def __init__(self, file, lr, hidden_dims, lmbd, C, std=1e-2, batch_size=64, epoch=100, verbose=0, N_train=None, \
+    def __init__(self, file, lr, hidden_dims, lmbd, C, std=1e-2, batch_size=64, epoch=30, verbose=0, N_train=None, \
                  momentum=0.5, decay_rate=0.99):
         self.config = {"learning_rate" : lr, "momentum" : momentum, "decay_rate" : decay_rate}
         self.v = {}
@@ -152,23 +153,31 @@ class FCN:
             self.N_train = N_train
         self.X_train, self.X_test, self.y_train, self.y_test = \
             train_test_split(self.X[0:self.N_train, :], self.y[0:self.N_train])
-        mean_image = np.mean(self.X_train, axis=0)
         self.X_train = self.X_train.astype(np.float64)
         self.X_test = self.X_test.astype(np.float64)
-        self.X_train -= mean_image
-        self.X_test -= mean_image
+        X_scaler = StandardScaler(with_mean=True, with_std=True)
+        self.X_train = X_scaler.fit_transform(self.X_train)
+        self.X_test = X_scaler.transform(self.X_test)
+        #mean_image = np.mean(self.X_train, axis=0)
+        #self.X_train = self.X_train.astype(np.float64)
+        #self.X_test = self.X_test.astype(np.float64)
+        #self.X_train -= mean_image
+        #self.X_test -= mean_image
         self.epsilon = 1e-5
         self.input_size = self.X_train.T.shape[0]
 
         for i in range(self.num_layers):
             if i==0:
-                self.params["W" + str(i)] = np.sqrt(2/self.input_size) * np.random.randn(self.input_size, hidden_dims[i])
+                #self.params["W" + str(i)] = np.sqrt(2/self.input_size) * np.random.randn(self.input_size, hidden_dims[i])
+                self.params["W" + str(i)] = std * np.random.randn(self.input_size, hidden_dims[i])
                 self.params["b" + str(i)] = np.zeros(hidden_dims[i])
             elif i<self.num_layers-1:
-                self.params["W" + str(i)] = np.sqrt(2/hidden_dims[i-1]) * np.random.randn(hidden_dims[i-1], hidden_dims[i])
+                #self.params["W" + str(i)] = np.sqrt(2/hidden_dims[i-1]) * np.random.randn(hidden_dims[i-1], hidden_dims[i])
+                self.params["W" + str(i)] = std * np.random.randn(hidden_dims[i - 1], hidden_dims[i])
                 self.params["b" + str(i)] = np.zeros(hidden_dims[i])
             else:
-                self.params["W" + str(i)] = np.sqrt(2/hidden_dims[i-1]) * np.random.randn(hidden_dims[i-1], self.C)
+                #self.params["W" + str(i)] = np.sqrt(2/hidden_dims[i-1]) * np.random.randn(hidden_dims[i-1], self.C)
+                self.params["W" + str(i)] = std * np.random.randn(hidden_dims[i - 1], self.C)
                 self.params["b" + str(i)] = np.zeros(self.C)
 
     def fit(self, X, y=None):
@@ -220,8 +229,8 @@ class FCN:
             loss_story.append(loss)
             for ii in range(self.num_layers):
                 w, b = "W" + str(ii), "b" + str(ii)
-                self.params[w] = self.rmsprop(self.params[w], grad[w], w)
-                self.params[b] = self.rmsprop(self.params[b], grad[b], b)
+                self.params[w] = self.sgd_momentum(self.params[w], grad[w], w)
+                self.params[b] = self.sgd_momentum(self.params[b], grad[b], b)
 
             param_scale = np.linalg.norm(self.params["W0"].ravel())
             update_scale = np.linalg.norm(grad["W0"].ravel())
@@ -230,8 +239,8 @@ class FCN:
             if self.verbose and i % 10 == 0:
                 print('iteration %d / %d: loss %f' % (i, self.num_iter, loss))
             if i % iterations_per_epoch == 0:
-                self.config["learning_rate"] *= 1
-                train_loss = self.predict(self.X_train, self.y_train, switch=1)
+                self.config["learning_rate"] *= 0.95
+                train_loss = self.predict(self.X_train, self.y_train, N_train=1000, switch=1)
                 test_loss = self.predict(self.X_test, self.y_test, switch=1)
                 test_loss_story.append(test_loss)  # np.linalg.norm(self.W2))
                 train_loss_story.append(train_loss)  # np.linalg.norm(self.W2))
@@ -240,10 +249,12 @@ class FCN:
             #self.progress(n)
         return loss_story, test_loss_story, train_loss_story, weight_scale_story
 
-    def predict(self, X, y, switch=None):
-        y_pred = self.fit(X)
+    def predict(self, X, y, N_train = None, switch=None):
+        if N_train is None:
+            N_train = X.shape[0]
+        y_pred = self.fit(X[:N_train])
         y_pred_10 = y_pred.argmax(1)
-        test_acc = accuracy_score(y_pred_10, y)
+        test_acc = accuracy_score(y_pred_10, y[:N_train])
         if (switch is None):
             print('\nTest Accuracy', test_acc)
             return y_pred_10
@@ -277,8 +288,8 @@ class FCN:
 
 
 
-ann_clf = FCN(file="./cifar-10-batches-py/data_batch_1", lr=1e-3, hidden_dims = [100], lmbd=0.05, C=10, \
-              batch_size=100, epoch=30, verbose=0, std=1e-3, N_train=10000, momentum=0.5, decay_rate=0.999)
+ann_clf = FCN(file="./cifar-10-batches-py/data_batch_2", lr=5e-3, hidden_dims = [100], lmbd=0.03, C=10, \
+              batch_size=100, epoch=50, verbose=0, std=1e-5, N_train=10000, momentum=0.9, decay_rate=0.999)
 
 #ann_clf.test()
 
