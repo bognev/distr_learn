aa097ea3eaf99036a9820f51776db8fb8d51bf9e
bognev <bognev@mail.ru>
2019-12-02 22:55:03 +0300

    Dropout

--- ann_ml.py
+++ ann_ml.py
@@ -96,9 +96,9 @@ class FCN:
         dx, dw, db = self.affine_backward(da, fc_cache)
         return dx, dw, db
 
-    def affine_batchnorm_relu_forward(self, x, w, b, gamma, betta, bn_param):
+    def affine_batchnorm_relu_forward(self, x, w, b, gamma, betta, mode, name):
         z_fc, cache_fc = self.affine_forward(x, w, b)
-        z_bn, cache_bn = self.batch_norm_forward(z_fc, gamma, betta, bn_param)
+        z_bn, cache_bn = self.batch_norm_forward(z_fc, gamma, betta, mode, name)
         z_relu, cache_relu = self.relu_forward(z_bn)
         cache = (cache_fc, cache_bn, cache_relu)
         return z_relu, cache
@@ -110,11 +110,15 @@ class FCN:
         dx, dw, db = self.affine_backward(dbn, fc_cache)
         return dx, dw, db, dgamma, dbetta
 
-    def batch_norm_forward(self, x, gamma, betta, bn_param):
+    def batch_norm_forward(self, x, gamma, betta, mode, name):
         cache = None
-        running_mean = bn_param.get('running_mean', np.zeros(D, dtype=x.dtype))
-        running_var = bn_param.get('running_var', np.zeros(D, dtype=x.dtype))
-        if bn_param["mode"] == "train":
+        if self.bn_param.get('running_mean' + name) is None:
+            self.bn_param['running_mean'+ name] = np.zeros_like(x.shape[0])
+            self.bn_param['running_var' + name] = np.zeros_like(x.shape[0])
+
+        running_mean = self.bn_param.get('running_mean'+ name)
+        running_var = self.bn_param.get('running_var' + name)
+        if mode == "train":
             mu = np.sum(x, axis=0)/x.shape[0]
             sigma = np.sum(np.power(x - mu,2), axis=0)/x.shape[0]
             x_bn = (x - mu)/np.sqrt(sigma + self.epsilon)
@@ -123,12 +127,25 @@ class FCN:
             running_var = 0.9 * running_var + (1 - 0.9) * sigma
             cache = (x, x_bn, mu, sigma, gamma, betta)
         else:
-            x_bn = (x - self.running_mean) / np.sqrt(self.running_var + self.epsilon)
+            x_bn = (x - running_mean) / np.sqrt(running_var + self.epsilon)
             y = gamma * x_bn + betta
-        bn_param['running_mean'] = running_mean
-        bn_param['running_var'] = running_var
+        self.bn_param['running_mean'+name] = running_mean
+        self.bn_param['running_var'+name] = running_var
 
         return y, cache
+
+    def dropout_forward(self, x, p, mode):
+        if mode == 'train':
+            r = np.random.binomial(1, p, x.shape)/p
+            y = r*x
+        else:
+            y = x
+        cache = (x, r)
+        return y, cache
+
+    def dropout_backward(self, dout, cache):
+        x, r = cache
+        return dout*r
 
     def batch_norm_backward(self, dout, cache):
         x, x_bn, mu, sigma, gamma, betta = cache
@@ -175,14 +192,14 @@ class FCN:
         return w
 
     def __init__(self, file, lr, hidden_dims, lmbd, C, std=1e-2, batch_size=64, epoch=30, verbose=0, N_train=None, \
-                 momentum=0.5, decay_rate=0.99, normalization=1):
+                 momentum=0.5, decay_rate=0.99, normalization=1, dropout=1):
         self.config = {"learning_rate" : lr, "momentum" : momentum, "decay_rate" : decay_rate}
         self.normalization = normalization
+        self.dropout = dropout
         self.v = {}
-        self.use_batchnorm = None
-        self.use_dropout = None
         self.num_layers = 1 + len(hidden_dims)
         self.params = {}
+        self.bn_param = {}
         self.cache = {}
         self.verbose = verbose
         self.lr = lr
@@ -194,8 +211,6 @@ class FCN:
         self.output_size = self.C
         self.X = np.array(self.batch[b'data'])
         self.y = np.array(self.batch[b'labels'])
-        self.running_mean = 0
-        self.running_var = 0
         if N_train is None:
             self.N_train = len(self.y)
         else:
@@ -212,27 +227,28 @@ class FCN:
         #self.X_test = self.X_test.astype(np.float64)
         #self.X_train -= mean_image
         #self.X_test -= mean_image
-        self.epsilon = 1e-8
+        self.epsilon = 1e-4
         self.input_size = self.X_train.T.shape[0]
 
         for i in range(self.num_layers):
             if i==0:
-                #self.params["W" + str(i)] = np.sqrt(2/self.input_size) * np.random.randn(self.input_size, hidden_dims[i])
-                self.params["W" + str(i)] = std * np.random.randn(self.input_size, hidden_dims[i])
+                self.params["W" + str(i)] = np.sqrt(2/self.input_size) * np.random.randn(self.input_size, hidden_dims[i])
+                # self.params["W" + str(i)] = std * np.random.randn(self.input_size, hidden_dims[i])
                 self.params["b" + str(i)] = np.zeros(hidden_dims[i])
                 if self.normalization == 1:
                     self.params["gamma" + str(i)] = np.ones(hidden_dims[i])
                     self.params["betta" + str(i)] = np.zeros(hidden_dims[i])
+
             elif i<self.num_layers-1:
-                #self.params["W" + str(i)] = np.sqrt(2/hidden_dims[i-1]) * np.random.randn(hidden_dims[i-1], hidden_dims[i])
-                self.params["W" + str(i)] = std * np.random.randn(hidden_dims[i - 1], hidden_dims[i])
+                self.params["W" + str(i)] = np.sqrt(2/hidden_dims[i-1]) * np.random.randn(hidden_dims[i-1], hidden_dims[i])
+                # self.params["W" + str(i)] = std * np.random.randn(hidden_dims[i - 1], hidden_dims[i])
                 self.params["b" + str(i)] = np.zeros(hidden_dims[i])
                 if self.normalization == 1:
                     self.params["gamma" + str(i)] = np.ones(hidden_dims[i])
                     self.params["betta" + str(i)] = np.zeros(hidden_dims[i])
             else:
-                #self.params["W" + str(i)] = np.sqrt(2/hidden_dims[i-1]) * np.random.randn(hidden_dims[i-1], self.C)
-                self.params["W" + str(i)] = std * np.random.randn(hidden_dims[i - 1], self.C)
+                self.params["W" + str(i)] = np.sqrt(2/hidden_dims[i-1]) * np.random.randn(hidden_dims[i-1], self.C)
+                # self.params["W" + str(i)] = std * np.random.randn(hidden_dims[i - 1], self.C)
                 self.params["b" + str(i)] = np.zeros(self.C)
 
     def fit(self, X, y=None):
@@ -247,9 +263,11 @@ class FCN:
             if i < self.num_layers - 1:
                 if self.normalization:
                     gamma, betta = self.params["gamma" + str(i)], self.params["betta" + str(i)]
-                    a[l], self.cache[l] = self.affine_batchnorm_relu_forward(a[l_prev], W, b, gamma, betta, bn_param)
+                    a[l], self.cache[l] = self.affine_batchnorm_relu_forward(a[l_prev], W, b, gamma, betta, mode, "layer" + str(i))
                 else:
                     a[l], self.cache[l] = self.affine_relu_forward(a[l_prev], W, b)
+                if self.dropout:
+                    a[l], self.cache[l] = self.dropout_forward(a[l], 0.5, mode)
             else:
                 a[l], self.cache[l] = self.affine_forward(a[l_prev], W, b)
 
@@ -258,7 +276,7 @@ class FCN:
         if y is None:
             return out
 
-        loss, grad, delta  = 0, {}, {}
+        loss, grad, dout  = 0, {}, {}
         loss, dout = self.softmax_loss(out, y)
         for i in range(self.num_layers):
             w = "W"+str(i)
@@ -269,14 +287,16 @@ class FCN:
             w, b = "W" + str(i), "b" + str(i)
             gamma, betta = "gamma" + str(i), "betta" + str(i)
             if i==self.num_layers-1:
-                delta[l_prev], grad[w], grad[b] = self.affine_backward(dout, self.cache[l])
+                dout[l_prev], grad[w], grad[b] = self.affine_backward(dout, self.cache[l])
             elif i<self.num_layers-1:
+                if self.dropout == 1:
+                    dout[l] = self.dropout_backward(dout[l], self.cache[l])
                 if self.normalization:
-                    delta[l_prev], grad[w], grad[b], grad[gamma], grad[betta] = \
-                        self.affine_batchnorm_relu_backward(delta[l], self.cache[l])
+                    dout[l_prev], grad[w], grad[b], grad[gamma], grad[betta] = \
+                        self.affine_batchnorm_relu_backward(dout[l], self.cache[l])
                 else:
-                    delta[l_prev], grad[w], grad[b] = \
-                        self.affine_relu_backward(delta[l], self.cache[l])
+                    dout[l_prev], grad[w], grad[b] = \
+                        self.affine_relu_backward(dout[l], self.cache[l])
 
         for i in range(self.num_layers):
             w = "W" + str(i)
@@ -298,12 +318,12 @@ class FCN:
             loss_story.append(loss)
             for ii in range(self.num_layers):
                 w, b = "W" + str(ii), "b" + str(ii)
-                self.params[w] = self.sgd_momentum(self.params[w], grad[w], w)
-                self.params[b] = self.sgd_momentum(self.params[b], grad[b], b)
+                self.params[w] = self.rmsprop(self.params[w], grad[w], w)
+                self.params[b] = self.rmsprop(self.params[b], grad[b], b)
                 if ii<self.num_layers-1 and self.normalization == 1:
                     gamma, betta = "gamma" + str(ii), "betta" + str(ii)
-                    self.params[gamma] = self.sgd(self.params[gamma], grad[gamma])
-                    self.params[betta] = self.sgd(self.params[betta], grad[betta])
+                    self.params[gamma] = self.rmsprop(self.params[gamma], grad[gamma], gamma)
+                    self.params[betta] = self.rmsprop(self.params[betta], grad[betta], betta)
 
             param_scale = np.linalg.norm(self.params["W0"].ravel())
             update_scale = self.config["learning_rate"]*np.linalg.norm(grad["W0"].ravel())/grad["W0"].ravel().shape[0]
@@ -312,12 +332,12 @@ class FCN:
             if self.verbose and i % 10 == 0:
                 print('iteration %d / %d: loss %f' % (i, self.num_iter, loss))
             if i % iterations_per_epoch == 0:
-                self.config["learning_rate"] *= 0.9
+                self.config["learning_rate"] *= 0.7
                 train_loss = self.predict(self.X_train, self.y_train, N_train=1000, switch=1)
                 test_loss = self.predict(self.X_test, self.y_test, switch=1)
                 test_loss_story.append(test_loss)  # np.linalg.norm(self.W2))
                 train_loss_story.append(train_loss)  # np.linalg.norm(self.W2))
-                print('Epoch %d / %d: train_acc %f; test_acc %f' % (num_epoch, self.num_epochs, train_loss, test_loss))
+                print('Epoch %d / %d: train_acc %f; test_acc %f; lr %f' % (num_epoch, self.num_epochs, train_loss, test_loss, self.config["learning_rate"]))
                 num_epoch += 1
             #self.progress(n)
         return loss_story, test_loss_story, train_loss_story, weight_scale_story
@@ -361,8 +381,9 @@ class FCN:
 
 
 
-ann_clf = FCN(file="./cifar-10-batches-py/data_batch_2", lr=1e-2, hidden_dims = [50, 25], lmbd=0.001, C=10, \
-              batch_size=100, epoch=30, verbose=0, std=1e-3, N_train=10000, momentum=0.9, decay_rate=0.999, normalization=1)
+ann_clf = FCN(file="./cifar-10-batches-py/data_batch_2", lr=0.4e-3, hidden_dims = [100], lmbd=0.1, C=10, \
+              batch_size=100, epoch=30, verbose=0, std=1e-3, N_train=10000, momentum=0.99, decay_rate=0.99, \
+              normalization=1, dropout=1)
 
 #ann_clf.test()
 
