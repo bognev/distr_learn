176df3a1ffe5cffed060ad3c66b1b032670ac3cb
zabolotsky <bognev@mail.ru>
2019-12-11 17:01:18 +0000

    CNN testing

--- cnn_ml.py
+++ cnn_ml.py
@@ -13,7 +13,7 @@ style.use("ggplot")
 import sys
 
 
-class CCN:
+class CNN:
     def unpickle(self, file):
         with open(file, 'rb') as fo:
             batch = pickle.load(fo, encoding='bytes')
@@ -195,13 +195,15 @@ class CCN:
         w = w - lr * dw / (np.sqrt(self.v[name])+1e-8)
         return w
 
-    def __init__(self, file, lr, hidden_dims, lmbd, C, std=1e-2, batch_size=64, epoch=30, verbose=0, N_train=None, \
-                 momentum=0.5, decay_rate=0.99, normalization=1, dropout=1):
+    def __init__(self, file, lr, input_dim = (3,32,32), hidden_dim=100, num_filters=32, filter_size=7, lmbd=0.01, C=10, std=1e-2, batch_size=64, epoch=30, verbose=0, N_train=None, \
+                 momentum=0.5, decay_rate=0.99):
+        self.input_dim = input_dim
+        self.num_filters = num_filters
+        self.filter_size = filter_size
+        self.hidden_dim = hidden_dim
         self.config = {"learning_rate" : lr, "momentum" : momentum, "decay_rate" : decay_rate}
-        self.normalization = normalization
-        self.dropout = dropout
         self.v = {}
-        self.num_layers = 1 + len(hidden_dims)
+        self.num_layers = 3
         self.params = {}
         self.bn_param = {}
         self.cache = {}
@@ -217,7 +219,8 @@ class CCN:
         self.batch_size = batch_size
         self.num_epochs = epoch
         self.output_size = self.C
-        self.X = np.concatenate((self.batch1[b'data'], self.batch2[b'data'], self.batch3[b'data'], self.batch4[b'data']))#,
+        self.X = np.concatenate((self.batch1[b'data'], self.batch2[b'data'], \
+                                 self.batch3[b'data'], self.batch4[b'data']))#,
                                  # self.batch5[b'data'], ))
         self.y = np.concatenate((self.batch1[b'labels'], self.batch2[b'labels'], self.batch3[b'labels'], self.batch4[b'labels']))#,
                                  # self.batch5[b'labels'],))
@@ -232,53 +235,39 @@ class CCN:
         X_scaler = StandardScaler(with_mean=True, with_std=True)
         self.X_train = X_scaler.fit_transform(self.X_train)
         self.X_test = X_scaler.transform(self.X_test)
+        self.X_train = self.X_train.reshape((7500, 3,32,32))
+        self.X_test = self.X_test.reshape((2500, 3, 32, 32))
         #mean_image = np.mean(self.X_train, axis=0)
         #self.X_train = self.X_train.astype(np.float64)
         #self.X_test = self.X_test.astype(np.float64)
         #self.X_train -= mean_image
         #self.X_test -= mean_image
         self.epsilon = 1e-4
-        self.input_size = self.X_train.T.shape[0]
+        self.input_size = self.X_train.shape[0]
 
         for i in range(self.num_layers):
             if i==0:
-                self.params["W" + str(i)] = np.sqrt(2/self.input_size) * np.random.randn(self.input_size, hidden_dims[i])
-                # self.params["W" + str(i)] = std * np.random.randn(self.input_size, hidden_dims[i])
-                self.params["b" + str(i)] = np.zeros(hidden_dims[i])
-                if self.normalization == 1:
-                    self.params["gamma" + str(i)] = np.ones(hidden_dims[i])
-                    self.params["betta" + str(i)] = np.zeros(hidden_dims[i])
-
+                self.params["W" + str(i)] = np.sqrt(2/self.input_size) * np.random.randn(self.num_filters, self.input_dim[0], self.filter_size, self.filter_size)
+                self.params["b" + str(i)] = np.zeros(self.num_filters)
             elif i<self.num_layers-1:
-                self.params["W" + str(i)] = np.sqrt(2/hidden_dims[i-1]) * np.random.randn(hidden_dims[i-1], hidden_dims[i])
-                # self.params["W" + str(i)] = std * np.random.randn(hidden_dims[i - 1], hidden_dims[i])
-                self.params["b" + str(i)] = np.zeros(hidden_dims[i])
-                if self.normalization == 1:
-                    self.params["gamma" + str(i)] = np.ones(hidden_dims[i])
-                    self.params["betta" + str(i)] = np.zeros(hidden_dims[i])
+                self.params["W" + str(i)] = np.sqrt(2/self.num_filters*self.filter_size*self.filter_size) * np.random.randn(self.num_filters*self.filter_size*self.filter_size, self.hidden_dim)
+                self.params["b" + str(i)] = np.zeros(hidden_dim)
             else:
-                self.params["W" + str(i)] = np.sqrt(2/hidden_dims[i-1]) * np.random.randn(hidden_dims[i-1], self.C)
-                # self.params["W" + str(i)] = std * np.random.randn(hidden_dims[i - 1], self.C)
+                self.params["W" + str(i)] = np.sqrt(2/hidden_dim) * np.random.randn(self.hidden_dim, self.C)
                 self.params["b" + str(i)] = np.zeros(self.C)
 
     def fit(self, X, y=None):
+        conv_param = {'stride': 2, 'padding': 1}
+        pool_param = {'stride': 2, 'pool_height': 2, 'pool_width': 2}
         a = {"layer0" : X}
-        if y is None:
-            mode="test"
-        else:
-            mode="train"
         for i in range(self.num_layers):
             l, l_prev = 'layer' + str(i + 1), 'layer' + str(i)
             W, b = self.params["W"+str(i)], self.params["b"+str(i)]
-            if i < self.num_layers - 1:
-                if self.normalization:
-                    gamma, betta = self.params["gamma" + str(i)], self.params["betta" + str(i)]
-                    a[l], self.cache[l] = self.affine_batchnorm_relu_forward(a[l_prev], W, b, gamma, betta, mode, "layer" + str(i))
-                else:
-                    a[l], self.cache[l] = self.affine_relu_forward(a[l_prev], W, b)
-                if self.dropout:
-                    a[l], self.cache[l] = self.dropout_forward(a[l], 0.5, mode, self.cache[l])
-            else:
+            if i == 0:
+                a[l], self.cache[l] = self.conv_relu_pool_forward(a[l_prev], W, b, conv_param, pool_param)
+            if i == 1:
+                a[l], self.cache[l] = self.affine_relu_forward(a[l_prev].reshape((self.batch_size,32*7*7)), W, b)
+            if i == 2:
                 a[l], self.cache[l] = self.affine_forward(a[l_prev], W, b)
 
         out = a["layer"+str(self.num_layers)]
@@ -295,18 +284,12 @@ class CCN:
         for i in reversed(range(self.num_layers)):
             l, l_prev = 'layer' + str(i + 1), 'layer' + str(i)
             w, b = "W" + str(i), "b" + str(i)
-            gamma, betta = "gamma" + str(i), "betta" + str(i)
-            if i==self.num_layers-1:
+            if i == 0:
+                dout[l_prev], grad[w], grad[b] = self.conv_relu_pool_backward(dout[l].reshape((self.batch_size,32,7,7)), self.cache[l])
+            if i == 1:
+                dout[l_prev], grad[w], grad[b] = self.affine_relu_backward(dout[l], self.cache[l])
+            if i == 2:
                 dout[l_prev], grad[w], grad[b] = self.affine_backward(delta, self.cache[l])
-            elif i<self.num_layers-1:
-                if self.dropout:
-                    dout[l], self.cache[l] = self.dropout_backward(dout[l], self.cache[l])
-                if self.normalization:
-                    dout[l_prev], grad[w], grad[b], grad[gamma], grad[betta] = \
-                        self.affine_batchnorm_relu_backward(dout[l], self.cache[l])
-                else:
-                    dout[l_prev], grad[w], grad[b] = \
-                        self.affine_relu_backward(dout[l], self.cache[l])
 
         for i in range(self.num_layers):
             w = "W" + str(i)
@@ -330,10 +313,6 @@ class CCN:
                 w, b = "W" + str(ii), "b" + str(ii)
                 self.params[w] = self.nesterov_momentum(self.params[w], grad[w], w)
                 self.params[b] = self.nesterov_momentum(self.params[b], grad[b], b)
-                if ii<self.num_layers-1 and self.normalization == 1:
-                    gamma, betta = "gamma" + str(ii), "betta" + str(ii)
-                    self.params[gamma] = self.nesterov_momentum(self.params[gamma], grad[gamma], gamma)
-                    self.params[betta] = self.nesterov_momentum(self.params[betta], grad[betta], betta)
 
             param_scale = np.linalg.norm(self.params["W0"].ravel())
             update_scale = self.config["learning_rate"]*np.linalg.norm(grad["W0"].ravel())/grad["W0"].ravel().shape[0]
@@ -343,8 +322,9 @@ class CCN:
                 print('iteration %d / %d: loss %f' % (i, self.num_iter, loss))
             if i % iterations_per_epoch == 0:
                 self.config["learning_rate"] *= 0.85
-                train_loss = self.predict(self.X_train, self.y_train, N_train=1000, switch=1)
-                test_loss = self.predict(self.X_test, self.y_test, switch=1)
+                train_loss, test_loss = 0,0
+                #train_loss = self.predict(self.X_train, self.y_train, N_train=1000, switch=1)
+                #test_loss = self.predict(self.X_test, self.y_test, switch=1)
                 test_loss_story.append(test_loss)  # np.linalg.norm(self.W2))
                 train_loss_story.append(train_loss)  # np.linalg.norm(self.W2))
                 print('Epoch %d / %d: train_acc %f; test_acc %f; lr %f' % (num_epoch, self.num_epochs, train_loss, test_loss, self.config["learning_rate"]))
@@ -416,7 +396,7 @@ class CCN:
         #                     out[n,k,ix,iy] = out[n,k,ix,iy] + np.sum(w[k,c]*x[n,c][np.ix_(np.arange(ix*S,ix*S+F).tolist(),np.arange(iy*S,iy*S+F).tolist())])
         cache = (x, w, b, conv_params)
 
-        return x, cache
+        return out, cache
 
     def conv_backward_naive(self, dout, cache):
         x, w, b, conv_params = cache
@@ -424,35 +404,92 @@ class CCN:
         _, C, F, _ = w.shape  # #filters, #channels, receptive field width, receptive field height
         P = conv_params['padding']
         S = conv_params['stride']
-        dx, dw, db = np.zeros_like(x), np.zeros_like(w), np.zeros_like(b)
         x_pad = np.pad(x, ((0, 0), (0, 0), (P, P), (P, P)), mode='constant', constant_values=0)
+        dx_pad = np.zeros_like(x_pad)#images, #channels, width, height
+        dw = np.zeros_like(w)#filters, #channels, field width, field height
+        db = np.zeros_like(b)
 
         for n in range(N): #over images
             for k in range(K): #over filters
-                for c in range(C): #over channels in image
-                    for ix in range(W_out):
-                        for iy in range(H_out):
-                            dw[k,c,ix,iy] += np.sum(dout[n,k,ix,iy]*x_pad[np.ix_(n,c,np.arange(ix*S,ix*S+F),np.arange(iy*S,iy*S+F))])
+                for ix in range(W_out):
+                    for iy in range(H_out):
+                        dw[k,:,:,:] += dout[n,k,ix,iy]*x_pad[n][np.ix_(np.arange(C), np.arange(ix*S,ix*S+F),np.arange(iy*S,iy*S+F))] #equal to convolution X and dL/dO
+                        db[k] += dout[n,k,ix,iy]
+                        dx_pad[n][np.ix_(np.arange(C), np.arange(ix*S,ix*S+F),np.arange(iy*S,iy*S+F))] += w[k,:,:,:] * dout[n,k,ix,iy] #equal to full convolution with W inverted
+        #https://medium.com/@pavisj/convolutions-and-backpropagations-46026a8f5d2c
+        dx = dx_pad[:, :, P:-P, P:-P]
 
         return dx, dw, db
+
+    def max_pool_forward_naive(self, x, pool_param):
+        PH, PW = pool_param["pool_height"], pool_param["pool_width"]
+        S = pool_param["stride"]
+        N, C, H, W = x.shape
+        H_out = int((H - PH) / S + 1)
+        W_out = int((W - PW) / S + 1)
+        out_shape = (N, C, H_out, W_out)
+        out = np.zeros(out_shape)
+        for n in range(N): #over images
+            for ix in range(W_out):
+                for iy in range(H_out):
+                    out[n,:,ix,iy] = np.amax(x[n][np.ix_(np.arange(C),np.arange(ix*S,ix*S+PH),np.arange(iy*S,iy*S+PW))], axis=(1,2))
+
+        cache = (x, pool_param)
+        return out, cache
+
+    def max_pool_backward(self, dout, cache):
+        x, pool_param = cache
+        PH, PW = pool_param["pool_height"], pool_param["pool_width"]
+        S = pool_param["stride"]
+        N, C, H, W = x.shape
+        _, _, H_out, W_out = dout.shape
+        dx = np.zeros_like(x)
+        for n in range(N): #over images
+            for c in range(C):
+                for ix in range(W_out):
+                    for iy in range(H_out):
+                        max_index = np.unravel_index((x[n,c][np.ix_(np.arange(ix*S,ix*S+PH),np.arange(iy*S,iy*S+PW))]).argmax(), \
+                                                     (x[n,c][np.ix_(np.arange(ix*S,ix*S+PH),np.arange(iy*S,iy*S+PW))]).shape)
+                        max_index = np.array(max_index)+np.array((ix*S,iy*S))
+                        dx[n,c,max_index[0],max_index[1]] = dout[n, c, ix, iy]
+
+        return dx
+
+    def conv_relu_pool_forward(self, x, w, b, conv_param, pool_param):
+        z_conv, cache_conv = self.conv_forward_naive(x, w, b, conv_param)
+        z_relu, cache_relu = self.relu_forward(z_conv)
+        z_pool, cache_pool = self.max_pool_forward_naive(z_relu, pool_param)
+        cache = (cache_conv, cache_relu, cache_pool)
+
+        return z_pool, cache
+
+    def conv_relu_pool_backward(self, dout, cache):
+        cache_conv, cache_relu, cache_pool = cache
+        dout_pool = self.max_pool_backward(dout, cache_pool)
+        dout_relu = self.relu_backward(dout_pool, cache_relu)
+        dx, dw, db = self.conv_backward_naive(dout_relu, cache_conv)
+
+        return dx, dw, db
+
+
 
 def rel_error(x, y):
   return np.max(np.abs(x - y) / (np.maximum(1e-8, np.abs(x) + np.abs(y))))
 
 
-cnn_clf = CCN(file="./cifar-10-batches-py/data_batch_", lr=1e-3, hidden_dims = [100], lmbd=0.01, C=10, \
-              batch_size=64, epoch=40, verbose=0, std=1e-3, N_train=10000, momentum=0.99, decay_rate=0.99, \
-              normalization=1, dropout=1)
+cnn_clf = CNN(file="./cifar-10-batches-py/data_batch_", lr=1e-3, hidden_dim = 100, lmbd=0.01, C=10, \
+              batch_size=8, epoch=40, verbose=0, std=1e-3, N_train=10000, momentum=0.99, decay_rate=0.99)
 
 #ann_clf.test()
-x_shape = (5, 3, 5, 5)
-w_shape = (2, 3, 3, 3)
-x = np.linspace(-0.1, 0.5, num=np.prod(x_shape)).reshape(x_shape)
-w = np.linspace(-0.2, 0.3, num=np.prod(w_shape)).reshape(w_shape)
-b = np.linspace(-0.1, 0.2, num=2)
+# x_shape = (4, 3, 7, 7)
+# w_shape = (2, 3, 3, 3)
+# x = np.linspace(-0.1, 0.5, num=np.prod(x_shape)).reshape(x_shape)
+# w = np.linspace(-0.2, 0.3, num=np.prod(w_shape)).reshape(w_shape)
+# b = np.linspace(-0.1, 0.2, num=2)
 
-conv_param = {'stride': 2, 'padding': 1}
-out, _ = cnn_clf.conv_forward_naive(x, w, b, conv_param)
+# conv_param = {'stride': 2, 'padding': 1}
+# pool_param = {'stride': 2, 'pool_height': 2, 'pool_width': 2}
+# out, cache = cnn_clf.conv_forward_naive(x, w, b, conv_param)
 # correct_out = np.array([[[[[-0.08759809, -0.10987781],
 #                            [-0.18387192, -0.2109216 ]],
 #                           [[ 0.21027089,  0.21661097],
@@ -465,7 +502,10 @@ out, _ = cnn_clf.conv_forward_naive(x, w, b, conv_param)
 #                            [ 0.59480972,  0.56776003]],
 #                           [[ 2.36270298,  2.36904306],
 #                            [ 2.38090835,  2.38247847]]]]])
-#loss, test_loss, train_loss, update = ann_clf.train()
+loss, test_loss, train_loss, update = cnn_clf.train()
+# pool_param = {'stride': 2, 'pool_height': 2, 'pool_width': 2}
+# out, cache = cnn_clf.max_pool_forward_naive(out, pool_param)
+# cnn_clf.max_pool_backward(out, cache)
 print("\n")
 print(loss[-1], test_loss[-1], train_loss[-1])
 
